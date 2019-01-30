c63ddf11bb4591f92ccaaeb2e21cbd766422cc25
Javier <jfernandil@alumnos.unex.es>
2019-01-30 02:41:26 +0100

    Fixing indentation
    Indentation has been fixed along whole script.

--- ip.sh
+++ ip.sh
@@ -17,62 +17,66 @@ LOG="/home/chema/ip-checker/logIP.log"
 
 
 
-	if [ -e $NOWIPADDR ] && [ -e $LOG ];
-	then
-		# Main block, here is checking if IP has changed
-        # then sends you an email
+if [ -e $NOWIPADDR ] && [ -e $LOG ];
+then
 
-        if [ "$(cat $NOWIPADDR)" != "$(curl -s $GETIPADDR)" ]
-        then
-            curl -s $GETIPADDR > $NOWIPADDR
-            # Switch here your personal mail to receive sent mail
-            echo "IP has changed to -> $(cat $NOWIPADDR)" | mail -s "YemaServer Ip has changed" WyRe12@gmail.com
-            {
-                date;
-                echo "IP has changed to -> $(cat $NOWIPADDR); sending email to WyRe12@gmail.com";
-                echo " ";
-            } >> $LOG
-		fi
+    # Main block, here is checking if IP has changed
+    # then sends you an email
 
-        # Debugging part to send mails or log info even
-        # when there are no changes
-
-        #if [ "$(cat $NOWIPADDR)" = "$(curl-s $GETIPADDR)" ]
-        #then
-		#	echo "Current IP is -> $(cat $NOWIPADDR)" | mail -s "Testing YemaServ mail Service" WyRe12@gmail.com
-        #    {
-        #        date;
-        #        echo "No IP change. Right now is $(cat $NOWIPADDR)";
-        #        echo " ";
-        #    } >> $LOG
-        #fi
-
-    # If nowipaddr or logIP.log does not exist it will creates needed file
-    # and it takes this as firs time execution.
-    else
-		if [ -e $LOG ]
-		then
-			rm $LOG
-		fi
-		if [ -e $NOWIPADDR ]
-		then
-			rm $NOWIPADDR
-		fi
-		touch $LOG
-        {
-            echo " ";
-            echo "##################################";
-            echo "Creating Log for script debugging:";
-            echo "##################################";
-            echo " ";
-        } >> $LOG
-
-        touch $NOWIPADDR
-		curl -s $GETIPADDR > $NOWIPADDR
-		echo "Fetching IP for the first time, IP -> $(cat $NOWIPADDR)" | mail -s "First time execution IP script" WyRe12@gmail.com
+    if [ "$(cat $NOWIPADDR)" != "$(curl -s $GETIPADDR)" ]
+    then
+        curl -s $GETIPADDR > $NOWIPADDR
+        # Switch here your personal mail to receive sent mail
+        echo "IP has changed to -> $(cat $NOWIPADDR)" | mail -s "YemaServer Ip has changed" WyRe12@gmail.com
         {
             date;
-            echo "Fetching IP for the first time, IP -> $(cat $NOWIPADDR); sending email to WyRe12@gmail.com";
+            echo "IP has changed to -> $(cat $NOWIPADDR); sending email to WyRe12@gmail.com";
             echo " ";
         } >> $LOG
-	fi
+    fi
+
+    # Debugging part to send mails or log info even
+    # when there are no changes
+
+    #if [ "$(cat $NOWIPADDR)" = "$(curl-s $GETIPADDR)" ]
+    #then
+    #    echo "Current IP is -> $(cat $NOWIPADDR)" | mail -s "Testing YemaServ mail Service" WyRe12@gmail.com
+    #    {
+    #        date;
+    #        echo "No IP change. Right now is $(cat $NOWIPADDR)";
+    #        echo " ";
+    #    } >> $LOG
+    #fi
+
+# If nowipaddr or logIP.log does not exist it will creates needed file
+# and it takes this as firs time execution.
+
+else
+
+    if [ -e $LOG ]
+    then
+        rm $LOG
+    fi
+    if [ -e $NOWIPADDR ]
+    then
+        rm $NOWIPADDR
+    fi
+
+    touch $LOG
+    {
+        echo " ";
+        echo "##################################";
+        echo "Creating Log for script debugging:";
+        echo "##################################";
+        echo " ";
+    } >> $LOG
+
+    touch $NOWIPADDR
+    curl -s $GETIPADDR > $NOWIPADDR
+    echo "Fetching IP for the first time, IP -> $(cat $NOWIPADDR)" | mail -s "First time execution IP script" WyRe12@gmail.com
+    {
+        date;
+        echo "Fetching IP for the first time, IP -> $(cat $NOWIPADDR); sending email to WyRe12@gmail.com";
+        echo " ";
+    } >> $LOG
+fi
